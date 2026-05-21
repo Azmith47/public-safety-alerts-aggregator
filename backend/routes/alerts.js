33ef0e7853ebc37938e5c087d29e5ed343d5c245
Azmith47 <ev_tv47@hotmail.com>
2026-05-21 21:19:28 +1000

    Add unsubscribe route to alerts API for token-based unsubscription

--- backend/routes/alerts.js
+++ backend/routes/alerts.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { getAlerts, getTrafficAlerts } = require('../controllers/alertsController');
-const { getAlertsFromDb, getAlertById } = require('../controllers/alertsController');
+const { getAlertsFromDb, getAlertById, unsubscribe } = require('../controllers/alertsController');
 
 router.get('/', getAlerts);
 router.get('/traffic', getTrafficAlerts);
 router.get('/db', getAlertsFromDb);
 router.get('/db/:id', getAlertById);
+router.get('/unsubscribe/:token', unsubscribe);
 
 module.exports = router;
