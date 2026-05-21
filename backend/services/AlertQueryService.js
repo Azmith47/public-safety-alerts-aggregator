1bb90ca6188153ff38e9b636840d3a8343d0c612
Azmith47 <ev_tv47@hotmail.com>
2026-05-21 21:35:12 +1000

    Fix import path for AlertMarkerDAO in AlertQueryService

--- backend/services/AlertQueryService.js
+++ backend/services/AlertQueryService.js
@@ -1,5 +1,5 @@
 const AlertDAO = require("../database/dao/AlertDAO");
-const AlertMarkerDAO = require("../database/dao/AlertMarkerDAO");
+const AlertMarkerDAO = require("../database/dao/AlertMarkersDAO");
 const AlertPolygonDAO = require("../database/dao/AlertPolygonDAO");
 const AlertRoadDAO = require("../database/dao/AlertRoadDAO");
 const AlertAdviceDAO = require("../database/dao/AlertAdviceDAO");
