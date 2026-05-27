c7463d59a1369382622ac1b0f3e3a98371f84bed
Azmith47 <ev_tv47@hotmail.com>
2026-05-27 23:44:51 +1000

    Reduced complexity from source collectors to just collect the data from the source and return a list of alerts in json form

--- backend/data-collection/api_collectors/rfsCollector.js
+++ backend/data-collection/api_collectors/rfsCollector.js
@@ -1,50 +1,23 @@
 import { FireAlert } from '../../models/alertClasses.js';
 import { geoJsonToPaths, geoJsonToMarker, splitDescription, parsePubDate } from '../../utils/alertUtilities.js';
+import CategoryDAO from '../../database/CategoryDAO.js';
 
 export const sourceName = 'NSW RFS';
 export const sourceWebsite = 'https://www.rfs.nsw.gov.au';
 
+const categoryCache = await CategoryDAO.getAll().then(categories => {
+    const cache = new Map();
+    categories.forEach(cat => cache.set(cat.name.toLowerCase(), cat.id));
+    return cache;
+});
+
 export const run = async () => {
-    const feedURL = 'https://www.rfs.nsw.gov.au/feeds/majorIncidents.json';
+    const FEED_URL = 'https://www.rfs.nsw.gov.au/feeds/majorIncidents.json';
     try {
-        const response = await fetch(feedURL);
-        const data = await response.json();
-        const alerts = [];
-
-        data?.features.forEach(feature => {
-            const title = feature?.properties?.title || 'No title';
-            const description = feature?.properties?.description || 'No description';
-            const link = feature?.properties?.link || 'No link';
-            const pubDate = parsePubDate(feature?.properties?.pubDate) || 'No publication date';
-            const alertLevel = feature?.properties?.category || 'No alert level';
-            const guuid = feature?.properties?.guid || null;
-            let id = null;
-            let markerPoint = null;
-            let polygon = null;
-
-            const splitGuuid = guuid.split('/');
-            id = splitGuuid[6] || null;
-
-            const markerPointGeoJson = feature?.geometry?.geometries?.[0] || feature?.geometry || null;
-            const polygonGeoJson = feature?.geometry?.geometries?.[1]?.geometries?.[0] || null;
-
-            if (markerPointGeoJson != null) {
-                markerPoint = geoJsonToMarker(markerPointGeoJson);
-            }
-
-            if (polygonGeoJson != null) {
-                polygon = geoJsonToPaths(polygonGeoJson);
-            }
-
-            let { location, councilArea, size, fire, agency, lastUpdated, status, category } = splitDescription(description);
-
-            lastUpdated = new Date(lastUpdated);
-
-            const alert = new FireAlert(title, link, pubDate, alertLevel, status, markerPoint, polygon, category, location, councilArea, size, fire, agency, lastUpdated, id);
-            alerts.push(alert);
-        });
-
-        return alerts;
+        const response = await fetch(FEED_URL);
+        const data =  await response.json();
+        return data?.features || [];
+        
     } catch (err) {
         console.error('RFS Collector error:', err.message);
         return [];
--- backend/data-collection/api_collectors/tfnswCollector.js
+++ backend/data-collection/api_collectors/tfnswCollector.js
@@ -26,56 +26,15 @@ export const run = async () => {
         for (const apiURL of apiURLs) {
             const response = await fetch(apiURL, {
                 headers: {
-                    'Authorization': `apikey ${apiKey}`
+                    'Authorization': `apikey ${apiKey}`,
+                    'Accept': 'application/json'
                 }
             });
             const data = await response.json();
-
-            data?.features.forEach(feature => {
-                const title = feature?.properties?.displayName || 'No title';
-                const id = feature?.id || null;
-                const link = 'https://www.livetraffic.com/incident-details/' + id || 'No link';
-                const pubDate = new Date(feature?.properties?.created) || null;
-
-                const markerPointGeoJson = feature?.geometry || null;
-                let markerPoint = null;
-
-                const polyline = feature?.properties?.encodedPolylines || null;
-                const lastUpdated = new Date(feature?.properties?.lastUpdated) || null;
-                const category = feature?.properties?.mainCategory || 'No category';
-
-                const plannedString = feature?.properties?.incidentKind || null;
-                const planned = plannedString === 'Planned' ? true : false;
-
-                const startDate = new Date(feature?.properties?.start) || null;
-                const endDate = new Date(feature?.properties?.end) || null;
-                const ended = feature?.properties?.ended || false;
-                const delay = feature?.properties?.delay || 0;
-                const headline = feature?.properties?.headline || null;
-                const impactingNetwork = feature?.properties?.impactingNetwork || false;
-                const isMajor = feature?.properties?.isMajor || false;
-                const queueLength = feature?.properties?.queueLength || 0;
-                const roads = feature?.properties?.roads || null;
-                const speedLimit = feature?.properties?.speedLimit || 0;
-                const subCategory = feature?.properties?.subCategoryA || null;
-                const otherLinks = feature?.properties?.webLinks || null;
-                const diversions = feature?.properties?.diversions || null;
-                const attendingGroups = feature?.properties?.attendingGroups || null;
-
-                const adviceA = feature?.properties?.adviceA || null;
-                const adviceB = feature?.properties?.adviceB || null;
-                const adviceC = feature?.properties?.adviceC || null;
-                const advice = [adviceA, adviceB, adviceC];
-
-                if (markerPointGeoJson != null) {
-                    markerPoint = geoJsonToMarker(markerPointGeoJson);
-                }
-
-                const alert = new TrafficAlert(title, id, link, pubDate, markerPoint, polyline, lastUpdated, category, planned, startDate, endDate, ended, delay, headline, impactingNetwork, isMajor, queueLength, roads, speedLimit, subCategory, otherLinks, diversions, attendingGroups, advice);
-                alerts.push(alert);
-            });
+            alerts.push(data?.features || []);
         }
-        return alerts;
+        return alerts.flat();
+            
     } catch (err) {
         console.error('TFNSW Collector error:', err.message);
         return [];
