60e2bee820a82e513d447b96361d697d60f2ea53
Christian <cajfino@gmail.com>
2026-09-02 17:31:50 +1000

    updated detailed modal to HTML table

--- client/src/app/(components)/modals/DetailedModal.tsx
+++ client/src/app/(components)/modals/DetailedModal.tsx
@@ -10,115 +10,78 @@ import {
   formatDate,
   displayFormat,
 } from "@/app/lib/utils";
+import { table } from "console";
 
-function Firecard({ alert }: { alert: Alert }) {
+function TestCard({ alert }: { alert: Alert }) {
   return (
-    <div className="modal-grid">
-      <article className="modal-label">
-        <p>Location:</p>
-        <p>Postcode:</p>
-        <p>Council Area:</p>
-        <p>Region:</p>
-        <p>Issued:</p>
-        <p>Last update:</p>
-        <p>Source:</p>
-        <p>Planned:</p>
-        <p>Is Major:</p>
-        <p>Network Impacted:</p>
-        <p>Delay:</p>
-        <p>Start Date:</p>
-        <p>End Date:</p>
-      </article>
-      <article className="modal-data">
-        <p>{alert.location_name}</p>
-        <p>{alert.location_postcode ?? "n/a"}</p>
-        <p>{displayFormat(alert.location_council_area) ?? "n/a"}</p>
-        <p>{displayFormat(alert.location_region) ?? "n/a"}</p>
-        <p>{convertTime(alert.issued_at) ?? "n/a"}</p>
-        <p>{convertTime(alert.updated_at) ?? "n/a"}</p>
-        <p>
+    <table>
+      <tr>
+        <td>Location:</td>
+        <td>{alert.location_name}</td>
+      </tr>
+      <tr>
+        <td>Postcode:</td>
+        <td>{alert.location_postcode ?? "n/a"}</td>
+      </tr>
+      <tr>
+        <td>Council Area:</td>
+        <td>{displayFormat(alert.location_council_area) ?? "n/a"}</td>
+      </tr>
+      <tr>
+        <td>Region:</td>
+        <td>{displayFormat(alert.location_region) ?? "n/a"}</td>
+      </tr>
+      <tr>
+        <td>Issued:</td>
+        <td>{convertTime(alert.issued_at) ?? "n/a"}</td>
+      </tr>
+      <tr>
+        <td>Last update:</td>
+        <td>{convertTime(alert.updated_at) ?? "n/a"}</td>
+      </tr>
+      <tr>
+        <td>Source:</td>
+        <td>
           {alert.source_url ? (
             <a href={alert.source_url}>{alert.source_url}</a>
           ) : (
             "n/a"
           )}
-        </p>
-        <p>{alert.planned === true ? "True" : "False"}</p>
-        <p>{alert.is_major === true ? "True" : "False"}</p>
-        <p>{alert.impacting_network === true ? "True" : "False"}</p>
-        <p>{alert.delay === true ? "True" : "False"}</p>
-        <time dateTime={convertDate(alert.start_date) ?? ""}>
-          {formatDate(alert.start_date) ?? "n/a"}
-        </time>
-        <br />
-        <br />
-        <time dateTime={convertDate(alert.end_date) ?? ""}>
-          {formatDate(alert.end_date) ?? "n/a"}
-        </time>
-      </article>
-      <p>
-        Alert category: {alert.category_id} // Alert source: {alert.source_id}{" "}
-        {alert.source_id === 1
-          ? "This should be a fire alert"
-          : "This should be a traffic alert"}
-      </p>
-    </div>
-  );
-}
-
-function TrafficCard({ alert }: { alert: Alert }) {
-  return (
-    <div className="modal-grid">
-      <article className="modal-label">
-        <p>Location:</p>
-        <p>Postcode:</p>
-        <p>Council Area:</p>
-        <p>Region:</p>
-        <p>Issued:</p>
-        <p>Last update:</p>
-        <p>Source:</p>
-        <p>Planned:</p>
-        <p>Is Major:</p>
-        <p>Network Impacted:</p>
-        <p>Delay:</p>
-        <p>Start Date:</p>
-        <p>End Date:</p>
-      </article>
-      <article className="modal-data">
-        <p>{alert.location_name ?? "n/a"}</p>
-        <p>{alert.location_postcode ?? "n/a"}</p>
-        <p>{displayFormat(alert.location_council_area) ?? "n/a"}</p>
-        <p>{displayFormat(alert.location_region) ?? "n/a"}</p>
-        <p>{convertTime(alert.issued_at) ?? "n/a"}</p>
-        <p>{convertTime(alert.updated_at) ?? "n/a"}</p>
-        <p>
-          {alert.source_url ? (
-            <a href={alert.source_url}>{alert.source_url}</a>
-          ) : (
-            "n/a"
-          )}
-        </p>
-        <p>{alert.planned ? "True" : "False"}</p>
-        <p>{alert.is_major === true ? "True" : "False"}</p>
-        <p>{alert.impacting_network === true ? "True" : "False"}</p>
-        <p>{alert.delay === true ? "True" : "False"}</p>
-        <time dateTime={convertDate(alert.start_date) ?? ""}>
-          {formatDate(alert.start_date) ?? "n/a"}
-        </time>
-        <br />
-        <br />
-        <time dateTime={convertDate(alert.end_date) ?? ""}>
-          {formatDate(alert.end_date) ?? "n/a"}
-        </time>
-      </article>
-      <p>
-        Alert category: {alert.category_id} // Alert source: {alert.source_id}{" "}
-        //{" "}
-        {alert.source_id === 1
-          ? "This should be a fire alert"
-          : "This should be a traffic alert"}
-      </p>
-    </div>
+        </td>
+      </tr>
+      <tr>
+        <td>Planned:</td>
+        <td>{alert.planned === true ? "True" : "False"}</td>
+      </tr>
+      <tr>
+        <td>Is Major:</td>
+        <td>{alert.is_major === true ? "True" : "False"}</td>
+      </tr>
+      <tr>
+        <td>Network Impacted:</td>
+        <td>{alert.impacting_network === true ? "True" : "False"}</td>
+      </tr>
+      <tr>
+        <td>Delay:</td>
+        <td>{alert.delay === true ? "True" : "False"}</td>
+      </tr>
+      <tr>
+        <td>Start Date:</td>
+        <td>
+          <time dateTime={convertDate(alert.start_date) ?? ""}>
+            {formatDate(alert.start_date) ?? "n/a"}
+          </time>
+        </td>
+      </tr>
+      <tr>
+        <td>End Date:</td>
+        <td>
+          <time dateTime={convertDate(alert.end_date) ?? ""}>
+            {formatDate(alert.end_date) ?? "n/a"}
+          </time>
+        </td>
+      </tr>
+    </table>
   );
 }
 
@@ -157,21 +120,12 @@ export default function DetailedModal() {
         </button>
       </div>
       <h4>{displayFormat(alert?.title)}</h4>
-      {alert?.source_id === 2 ? (
-        <TrafficCard alert={alert} />
-      ) : (
-        <>
-          <Firecard alert={alert} />
-        </>
-      )}
+      <TestCard alert={alert} />
       <div className="subscribe-section">
         <p>Click to subscribe</p>
         <input
           type="checkbox"
-          //Re-render checkbox if title is in subscribedAlertTitles array
-          //Otherwise checkbox stays clicked for every alert
           checked={subscribedAlertTitles.includes(alert.title)}
-          //Click checkbox => if title is in subscribedAlertTitles array remove it, otherwise add it
           onChange={() =>
             subscribedAlertTitles.includes(alert.title)
               ? removeSubscribedAlert(alert.title)
